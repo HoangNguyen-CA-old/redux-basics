ab7ee1e7e44640f25507eccc0e54c93835ea91ff
HoangNguyen-CA <hoang.viet.nguyen@outlook.com>
2020-06-08 14:43:23 -0400

    added logging middleware for redux

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 
 import App from './App';
@@ -8,7 +8,20 @@ import './index.css';
 
 import reducer from './store/reducers/index';
 
-const store = createStore(reducer);
+//middleware
+const logger = (store) => {
+  return (next) => {
+    return (action) => {
+      console.log('[Middleware] action', action);
+      const result = next(action);
+      console.log('[Middleware] next state', store.getState());
+
+      return result;
+    };
+  };
+};
+
+const store = createStore(reducer, applyMiddleware(logger));
 
 ReactDOM.render(
   <React.StrictMode>
